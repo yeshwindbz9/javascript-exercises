ad2d3d6eab98a7fd2f58847881e8a8c2c8c3831b
yeshwindbz9 <yeshwindbz9@gmail.com>
2023-11-12 19:23:28 +0000

    08 complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,26 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((acc, curr)=> acc+curr, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	return arr.reduce((acc, curr)=> acc*curr, 1);
 };
 
-const power = function() {
-	
+const power = function(n, p) {
+	return Math.pow(n, p);
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if (number===1||number===0) return 1;
+  else return number * factorial(number-1);
 };
 
 // Do not edit below this line
