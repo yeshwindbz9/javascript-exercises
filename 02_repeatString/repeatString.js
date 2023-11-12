0b3bd90a8f45931bb42da4f4142bdb279be152e4
yeshwindbz9 <yeshwindbz9@gmail.com>
2023-11-12 14:34:41 +0000

    01, 02 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(str, count) {
+    if (count<0) return "ERROR";
+    let newStr= "";
+    while(count>0){
+        newStr += str;
+        count -=1;
+    }
+    return newStr;
 };
 
 // Do not edit below this line
