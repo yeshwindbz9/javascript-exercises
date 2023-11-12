c8d902e79c1afab9fa0904a7203eb2a3f2e6ab74
yeshwindbz9 <yeshwindbz9@gmail.com>
2023-11-12 17:17:12 +0000

    07 complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let temp_c = (temp -32)*5/9;
+  return Math.round((temp_c + Number.EPSILON) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let temp_f = (temp*9/5)+32;
+  return Math.round((temp_f + Number.EPSILON) * 10) / 10;
 };
 
 // Do not edit below this line
