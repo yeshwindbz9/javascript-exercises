3276b12acb8424f492a43430a4e9a1339464f5c3
yeshwindbz9 <yeshwindbz9@gmail.com>
2023-11-12 16:22:33 +0000

    05 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
-
+const sumAll = function(start, stop) {
+    if(typeof start !== "number" || typeof stop !== "number") return "ERROR";
+    else if (start<0 ||stop<0) return "ERROR";
+    else{
+        if (start>stop) [start, stop] = [stop, start];
+        let sum = 0;
+        while(start<=stop){
+            sum+=start;
+            start +=1;
+        }
+        return sum;
+    }
+    
 };
 
 // Do not edit below this line
