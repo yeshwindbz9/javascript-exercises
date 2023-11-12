eef8d92e63a8752d1511df8dc2419e971b539a5d
yeshwindbz9 <yeshwindbz9@gmail.com>
2023-11-12 15:05:36 +0000

    04 complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    let i = 0;
+    while(i<arr.length){
+        if(args.includes(arr[i])) arr.splice(i, 1);
+        else i+=1;
+    }
+    return arr;
 };
 
 // Do not edit below this line
