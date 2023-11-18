0fa0cd94195e419d21fa7d4a27052dafdedbbe2e
yeshwindbz9 <yeshwindbz9@gmail.com>
2023-11-18 22:45:29 +0000

    completed 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    n = Number(n);
+    if(n<1) return "OOPS";
+    let a=0, b=1, c, res=[1], i=1;
+    while(i<n){
+        c = a + b;
+        res.push(c);
+        a = b;
+        b = c;
+        i+=1;
+    }
+    return Number(res.slice(-1));
 };
 
 // Do not edit below this line
