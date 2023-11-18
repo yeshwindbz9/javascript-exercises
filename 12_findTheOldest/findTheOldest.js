a446c564e799744a81370763f9d33131e8416e3e
yeshwindbz9 <yeshwindbz9@gmail.com>
2023-11-18 22:58:59 +0000

    completed 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let oldestAge=0, oldestPerson= "", age=0;
+    const date = new Date();
+    for(let person of people){
+        if("yearOfDeath" in person) age = person.yearOfDeath - person.yearOfBirth;
+        else age = date.getFullYear() - person.yearOfBirth;
+        if (age>oldestAge) {
+            oldestAge = age;
+            oldestPerson = person;
+        }
+    }
+    return oldestPerson;
 };
 
 // Do not edit below this line
