531c817cb08f15ef78d62426bfcbef3fda6e16a6
yeshwindbz9 <yeshwindbz9@gmail.com>
2023-11-18 22:10:51 +0000

    completed 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let new_str = str.replace(/[^\w]|_/g, "").toLowerCase();
+    return new_str === new_str.split("").reverse().join("");
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+palindromes('A car, a man, a maraca.');
